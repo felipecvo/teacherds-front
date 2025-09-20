39b9c79cb5948014ff13081e09028ffa8b29aace
Prof. Felipe Vilella <proffelipe.oliveira@fiap.com.br>
2025-09-20 19:15:22 -0300

    chore: setup react router v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Index from "./routes";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+const Index = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-foreground">
+        Professor Dashboard
+      </h1>
+      <p className="text-gray-500 mt-2">
+        Welcome back! Manage your classrooms and evaluate student assessments.
+      </p>
+    </div>
+  );
+};
+export default Index;
